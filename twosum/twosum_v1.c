510ccc98b66c9f9ccf5dfca6dbfd4670a6ace151
gengermao <32906885+gengermao@users.noreply.github.com>
2017-10-22 18:59:38 +0800

    Understand the algorithm
    * the Time complexity is O(n)and space complexity is O(1)
    * the space complexity maybe not accuracy
    * the first statement in this function is used to deal with negative

--- twosum/twosum_v1.c
+++ twosum/twosum_v1.c
@@ -1,3 +1,8 @@
+/***
+* the Time complexity is O(n)and space complexity is O(1)
+* the space complexity maybe not accuracy
+* the first statement in this function is used to deal with negative
+***/
 int* twoSum(int* nums, int numsSize, int target) {
     int _[100001] = {0}, *index_plus_one = _ + 50000;
     for (int i = 0; i < numsSize; i++) {
