7826ca213b50d5048027aacb5cb321ade764c6c1
gengermao <32906885+gengermao@users.noreply.github.com>
2018-01-07 16:39:35 +0800

    Create myAtoI_v1.c

---
+++ atoi/myAtoI_v1.c
@@ -0,0 +1,25 @@
+int myAtoi(char* str) {
+    int i, n, sign, c;
+
+	for(i = 0; isspace(str[i]); i++)
+		;
+	sign = (str[i] == '-') ? -1 : 1;
+	if(str[i] == '+' || str[i] == '-')
+		i++;
+	for(n = 0; isdigit(str[i]); i++)
+	{
+		c = (str[i] - '0');
+        if (sign == 1 && (n > INT_MAX/10 || (n == INT_MAX/10 && c >= INT_MAX%10)))  
+        {  
+            return INT_MAX;  
+        }  
+        else if (sign == -1 && (n > (unsigned)INT_MIN/10 || (n == (unsigned)INT_MIN/10 && c >= (unsigned)INT_MIN%10)))  
+        {  
+            return INT_MIN;  
+        }
+        
+        n = n * 10 + c;
+	}
+
+	return sign*n; 
+}
